06b86ccd270e370ade45b4bd9f59b7178358d599
syoch <syoch64@gmail.com>
2023-03-24 12:36:38 +0900

    Update: Auto dependency resolver

--- install.sh
+++ install.sh
@@ -2,8 +2,19 @@
 
 cd $(dirname $0)
 
-mkdir -p ~/.local/share/MC-CTF || exit 0
+printf "\e[1;32mChecking for dependencies...\e[0m\n"
+printf "\e[33mDocker container\e[0m\n"
+docker image ls 2>/dev/null | grep -q mc_ctf
+if [ $? -ne 0 ]; then
+    printf "\e[2;33m Not found => doing Install...\e[0m\n"
+    git clone https://github.com/Mine-Code/ctf-runtime
+    cd ctf-runtime
+    make
+    cd ..
+fi
 
+
+mkdir -p ~/.local/share/MC-CTF || exit 0
 
 cp sdk/mc_ctf ~/.local/bin/mc_ctf
 chmod +x ~/.local/bin/mc_ctf
